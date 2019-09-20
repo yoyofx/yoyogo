1989328c946d619a94aea936fc83576180ac582e
yoyofx <zl.hxd@hotmail.com>
2019-09-20 22:13:55 +0800

    添加了ctx.bind(i interface{}) 函数的DEMO。

--- main.go
+++ main.go
@@ -4,20 +4,27 @@ import (
 	"fmt"
 	"github.com/maxzhang1985/yoyogo/Framework"
 	"github.com/maxzhang1985/yoyogo/Middleware"
-	"github.com/maxzhang1985/yoyogo/Standard"
+	Std "github.com/maxzhang1985/yoyogo/Standard"
 )
+
+type UserInfo struct {
+	UserName string `json:"username"`
+}
 
 func main() {
 
 	app := YoyoGo.UseMvc()
-	app.Use(Middleware.NewStatic("Static"))
+	app.UseStatic("Static")
 
 	app.Map("/info", func(ctx *Middleware.HttpContext) {
 
 		qs_q1 := ctx.Query("q1")
 		pd_name := ctx.Param("username")
 
-		strResult := fmt.Sprintf("Name:%s , Q1:%s", pd_name, qs_q1)
+		userInfo := &UserInfo{}
+		_ = ctx.Bind(userInfo)
+
+		strResult := fmt.Sprintf("Name:%s , Q1:%s , bind: %s", pd_name, qs_q1, userInfo.UserName)
 
 		ctx.JSON(Std.M{"info": "hello world", "result": strResult})
 	})
