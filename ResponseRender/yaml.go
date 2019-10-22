c186e14f622e9914ad8ca58640307b37d8b42709
张磊 <zl.hxd@hotmail.com>
2019-10-22 14:45:13 +0800

    YAML Response Render

---
+++ ResponseRender/yaml.go
@@ -0,0 +1,28 @@
+package ResponseRender
+
+import (
+	"gopkg.in/yaml.v2"
+	"net/http"
+)
+
+type YAML struct {
+	Data interface{}
+}
+
+var yamlContentType = []string{"application/x-yaml; charset=utf-8"}
+
+func (r YAML) WriteContentType(w http.ResponseWriter) {
+	writeContentType(w, yamlContentType)
+}
+
+func (r YAML) Render(w http.ResponseWriter) error {
+	r.WriteContentType(w)
+
+	bytes, err := yaml.Marshal(r.Data)
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(bytes)
+	return err
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/golang/protobuf v1.3.2
 	github.com/ugorji/go/codec v1.1.7
+	gopkg.in/yaml.v2 v2.2.4
 )
