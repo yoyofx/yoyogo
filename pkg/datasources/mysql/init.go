273d2794735703c4a61a94e6004bb82b72e44b16
yoyofx <zlhxd@hotmail.com>
2021-01-29 12:54:52 +0800

    mysql datasource

--- pkg/datasources/mysql/init.go
+++ pkg/datasources/mysql/init.go
@@ -3,6 +3,7 @@ package mysql
 import (
 	"github.com/yoyofx/yoyogo/abstractions"
 	"github.com/yoyofx/yoyogo/dependencyinjection"
+	//_ "gorm.io/gorm"
 )
 
 func init() {
@@ -10,4 +11,8 @@ func init() {
 		func(config abstractions.IConfiguration, serviceCollection *dependencyinjection.ServiceCollection) {
 			serviceCollection.AddSingletonByImplementsAndName("db1", NewMysqlDataSource, new(abstractions.IDataSource))
 		})
+
+	//gormDB, err := gorm.Open(mysql.New(mysql.Config{
+	//	Conn: sqlDB,
+	//}), &gorm.Config{})
 }
