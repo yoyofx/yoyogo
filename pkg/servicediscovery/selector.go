2d18bde64e5225e08cce9262a61ddd24c5e5b653
yoyofx <zlhxd@hotmail.com>
2021-02-20 19:22:13 +0800

    Selector

--- pkg/servicediscovery/selector.go
+++ pkg/servicediscovery/selector.go
@@ -9,7 +9,7 @@ type Selector struct {
 }
 
 // will set strategy and cache options
-
+// Selector( strategy ,  cache ).Select(serviceName).(ServiceInstance)
 func (s *Selector) Select(serviceName string) (servicediscovery.ServiceInstance, error) {
 	//service:= s.discoveryCache.GetService(serviceName)
 	//return  s.strategy.Next(service)
