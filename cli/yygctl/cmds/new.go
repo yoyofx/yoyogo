b982af66f673eb98a245a37217b1372ede2c8227
Artanis <>
2021-06-28 17:14:08 +0800

    修改cli

--- cli/yygctl/cmds/new.go
+++ cli/yygctl/cmds/new.go
@@ -4,15 +4,18 @@ import (
 	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/yoyofx/yoyogo/cli/yygctl/generate/projects"
 	"github.com/yoyofx/yoyogo/cli/yygctl/generate/templates"
 	"github.com/yoyofx/yoyogo/cli/yygctl/telplate"
 	"html/template"
+	"io/fs"
 	"os"
 	"strings"
 )
 
 var l bool
 var projectName string
+var path string
 var NewCmd = &cobra.Command{
 	Use:   "new",
 	Short: "create new yoyogo demo by template",
@@ -44,7 +47,7 @@ var NewCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		if !l {
 			fmt.Println("project:" + projectName)
-			createProjectMain()
+			createProject()
 		}
 
 	},
@@ -52,7 +55,43 @@ var NewCmd = &cobra.Command{
 
 func init() {
 	NewCmd.Flags().BoolVarP(&l, "templates", "l", false, "list of template")
+	NewCmd.Flags().StringVarP(&path, "path", "p", "", "dir path")
 	NewCmd.Flags().StringVarP(&projectName, "projectName", "n", "demo", "the name of project")
+}
+
+func createProject() {
+	project := projects.Project{
+		Name: projectName,
+		Path: path,
+		Dom: &projects.ProjectItem{
+			Name: projectName,
+			Type: projects.ProjectItemDir,
+		},
+	}
+	project.With(func(root *projects.ProjectItem) {
+		root.AddFileWithContent("main.go", telplate.ConsoleMainTel)
+		root.AddFileWithContent("startup.go", telplate.ConsoleStartUpTel)
+		root.AddFileWithContent("go.mod", telplate.ConsoleGoModTel)
+		root.AddFileWithContent("config.yml", telplate.ConsoleConfigTel)
+		root.AddFileWithContent("service.go", telplate.ConsoleServiceTel)
+	})
+	var dirPath string
+	if path == "" {
+		dirPath = projectName
+
+	} else {
+		dirPath = path + projectName
+	}
+	data := map[string]interface{}{
+		"ModelName": projectName,
+	}
+	os.Mkdir(dirPath, fs.ModeDir)
+	for _, x := range project.Dom.Dom {
+		tel, _ := template.New("demo").Parse(x.Content)
+		file, err := os.OpenFile(dirPath+"/"+x.Name, os.O_CREATE|os.O_WRONLY, 0755)
+		fmt.Println(err)
+		tel.Execute(file, data)
+	}
 }
 
 func createProjectMain() {
