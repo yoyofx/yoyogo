c9d75c343ded923c6dabbdfbd9b12ea0a86be8fb
Artanis <>
2021-07-06 15:14:55 +0800

    添加 grpc cli

--- cli/yygctl/generate/templates/grpc/service_go_tel.go
+++ cli/yygctl/generate/templates/grpc/service_go_tel.go
@@ -85,3 +85,73 @@ func (demo *IOCDemo) Print() string {
 	return "IOC Demo test"
 }
 `
+
+const ServiceConfig_Tel = `
+yoyogo:
+  application:
+    name: yoyogo_grpc_dev
+    metadata: "develop"
+    server:
+      type: "grpc"
+      address: ":31127"
+      tls:
+        cert: ""
+        key: ""
+  cloud:
+    apm:
+      skyworking:
+        address: localhost:11800
+    discovery:
+      cache:
+        ttl: 30     # seconds
+      type: "nacos"
+      metadata:
+        url: "120.53.133.30"
+        port: 80
+        namespace: "public"
+        group: ""
+        cluster: ""
+        auth:
+          enable: true
+          username: "root"
+          password: "1234"
+#      type: "consul"
+#      metadata:
+#        address: "localhost:8500"
+#        health_check: "/actuator/health"
+#        tags: [""]
+#      type: "eureka"
+#      metadata:
+#         datacenter: ""
+#         address:
+#           - "http://sd.yoyogo.run/eureka"
+#         ttl: 30   # seconds
+#      type: "etcd"
+#      metadata:
+#        address:
+#          - "81.70.154.55:32379"
+#        namespace: "public"
+#        ttl: 60
+#        auth:
+#          enable: true
+#          username: "root"
+#          password: "5z1fLbYw8A"
+  datasource:
+      pool:
+        init_cap: 2
+        max_cap: 5
+        idle_timeout: 5
+      db:
+        name: db1
+        type: mysql
+        url: tcp(cdb-amqub3mo.bj.tencentcdb.com:10042)/yoyoBlog?charset=utf8&parseTime=True
+        username: root
+        password: 1234abcd
+        debug: true
+      redis:
+        name: reids1
+        url: 62.234.6.120:31379
+        password:
+        db: 0
+
+`
