c6d905e2bc65021b2960836db154a48e5089c7a0
yoyofx <zlhxd@hotmail.com>
2021-07-20 16:28:33 +0800

    update mvc template unit tests

--- tests/mvc_routetemplate_test.go
+++ tests/mvc_routetemplate_test.go
@@ -13,17 +13,18 @@ func Test_MvcTemplate(t *testing.T) {
 	assert.Equal(t, template.GetControllerIndex(), 1)
 	assert.Equal(t, template.GetActionIndex(), 2)
 
-	assert.Equal(t, template.Match(strings.Split(url, "/")), true)
-	assert.Equal(t, template.ControllerName, "usercontroller")
-	assert.Equal(t, template.ActionName, "register")
+	matchInfo := &mvc.MatchMvcInfo{}
+	assert.Equal(t, template.Match(strings.Split(url, "/"), matchInfo), true)
+	assert.Equal(t, matchInfo.ControllerName, "usercontroller")
+	assert.Equal(t, matchInfo.ActionName, "register")
 
 	template1 := mvc.NewRouteTemplate("api/v1/{controller}/{action}")
 	assert.Equal(t, template1.GetControllerIndex(), 2)
 	assert.Equal(t, template1.GetActionIndex(), 3)
 
-	assert.Equal(t, template1.Match(strings.Split(url, "/")), false)
-	assert.Equal(t, template1.Match(strings.Split("api/"+url, "/")), true)
-	assert.Equal(t, template1.ControllerName, "usercontroller")
-	assert.Equal(t, template1.ActionName, "register")
+	assert.Equal(t, template1.Match(strings.Split(url, "/"), matchInfo), false)
+	assert.Equal(t, template1.Match(strings.Split("api/"+url, "/"), matchInfo), true)
+	assert.Equal(t, matchInfo.ControllerName, "usercontroller")
+	assert.Equal(t, matchInfo.ActionName, "register")
 
 }
