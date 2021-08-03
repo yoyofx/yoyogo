9dd1efd3b822c156a282031132931c74b3127265
yoyofx <zl.hxd@hotmail.com>
2021-08-03 16:45:50 +0800

    fx version updated

--- cli/yygctl/generate/projects/version.go
+++ cli/yygctl/generate/projects/version.go
@@ -2,7 +2,7 @@ package projects
 
 const (
 	//Application Version, such as v1.x.x pre-release
-	Version = "v1.7.5"
+	Version = "v1.7.7"
 	//Application logo
 	Logo = `
                                                 $$\     $$\                 $$\ $$\ 
