0843458cbce5ec14853f136495a38451f17169e0
yoyofx <zl.hxd@hotmail.com>
2021-12-30 22:32:30 +0800

    添加本地配置对象

--- examples/simpleweb/main.go
+++ examples/simpleweb/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/yoyofx/yoyogo/abstractions"
 	"github.com/yoyofx/yoyogo/abstractions/xlog"
 	"github.com/yoyofx/yoyogo/pkg/configuration"
-	nacosconfig "github.com/yoyofx/yoyogo/pkg/configuration/nacos"
 	_ "github.com/yoyofx/yoyogo/pkg/datasources/mysql"
 	_ "github.com/yoyofx/yoyogo/pkg/datasources/redis"
 	"github.com/yoyofx/yoyogo/pkg/servicediscovery/nacos"
@@ -45,8 +44,18 @@ func main() {
 
 //* Create the builder of Web host
 func CreateCustomBuilder() *abstractions.HostBuilder {
-	config := nacosconfig.RemoteConfig("config")
-	//config := apollo.RemoteConfig("config")
+	// local away:
+	//  config := abstractions.NewConfigurationBuilder().
+	//	AddEnvironment().
+	//	AddYamlFile("config").Build()
+	// remote away:
+	// 	-- github.com/yoyofx/yoyogo/pkg/configuration/nacos
+	// 	-- github.com/yoyofx/yoyogo/pkg/configuration/apollo
+	//  config := nacosconfig.RemoteConfig("config")
+	//  config := apollo.RemoteConfig("config")
+
+	config := configuration.LocalConfig("config")
+
 	return web.NewWebHostBuilder().
 		UseConfiguration(config).
 		Configure(func(app *web.ApplicationBuilder) {
