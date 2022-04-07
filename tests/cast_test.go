0efd31d27aa56e34ecbb48d0a121c982ecc0b1c2
yoyofx <zl.hxd@hotmail.com>
2022-04-07 18:15:33 +0800

    IFF func

--- tests/cast_test.go
+++ tests/cast_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/yoyofx/yoyogo/utils"
 	"github.com/yoyofx/yoyogo/utils/cast"
 	"testing"
 )
@@ -13,4 +14,11 @@ func TestStringToNumberConvert(t *testing.T) {
 	var outNum int64
 	_ = cast.Str2NPtr("1111", &outNum)
 	assert.Equal(t, outNum, int64(1111))
+}
+
+func TestCondition(t *testing.T) {
+	a := -1
+	assert.Equal(t, utils.IFF(a > 0, a, 0), 0)
+
+	assert.Equal(t, utils.IFN(a > 0, func() int { return a }, func() int { return 0 }), 0)
 }
