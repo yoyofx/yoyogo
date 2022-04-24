7ac7e63474f612679d77a7c488a0cf888452aa81
yoyofx <zl.hxd@hotmail.com>
2022-04-24 19:22:10 +0800

    get config value by environment

--- tests/config_test.go
+++ tests/config_test.go
@@ -1,21 +1,32 @@
 package tests
 
 import (
-	"fmt"
 	"github.com/yoyofx/yoyogo/abstractions"
 	"os"
 	"testing"
 )
 
+type Profile struct {
+	DNS string
+	IP  string
+}
+
 func TestEnv(t *testing.T) {
+
+	_ = os.Setenv("CUSTOM_ENV", "my env variable")
+	_ = os.Setenv("REMOTE_HOST", "my host")
+
 	config := abstractions.NewConfigurationBuilder().
 		AddEnvironment().
 		AddYamlFile("config").Build()
-	os.Setenv("HOMEBREW_BOTTLE_DOMAIN", "hello world")
-	str := config.Get("env")
-	str2 := config.Get("profile.homebrew")
-	str3 := config.Get("profile.default")
-	fmt.Println(str)
-	fmt.Println(str2)
-	fmt.Println(str3)
+
+	//str := config.Get("env")
+	//str2 := config.Get("profile.dns")
+	//str3 := config.Get("profile.ip")
+	//fmt.Println(str)
+	//fmt.Println(str2)
+	//assert.Equal(t, str3, "yoyogoDefault")
+	var profile Profile
+	config.GetConfigObject("profile", &profile)
+
 }
